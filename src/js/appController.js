1cc45e4225589c97916257f6205da9066b308680
Manuel <manviejogon@gmail.com>
2017-06-09 01:03:41 -0500

    Improved responsiveness, added autofill

--- src/js/appController.js
+++ src/js/appController.js
@@ -6,82 +6,100 @@
  * Your application specific code will go here
  */
 define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarraytabledatasource',
-  'ojs/ojoffcanvas'],
-  function(oj, ko) {
-     function ControllerViewModel() {
-       var self = this;
+        'ojs/ojoffcanvas'
+    ],
+    function(oj, ko) {
+        function ControllerViewModel() {
+            var self = this;
 
-      // Media queries for repsonsive layouts
-      var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
-      self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
-      var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
-      self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+            // Media queries for repsonsive layouts
+            var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+            self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+            var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+            self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
 
-       // Router setup
-      //  self.router = oj.Router.rootInstance;
-      //  self.router.configure({
-      //    'carousel': {label: 'Inicio', isDefault: true},
-      //    'quienessomos': {label: 'Quienes somos'},
-      //    'estaciones': {label: 'Estaciones'},
-      //    'pedidos': {label: 'Pedidos'}
-      //  });
-      // oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
+            // Router setup
+            //  self.router = oj.Router.rootInstance;
+            //  self.router.configure({
+            //    'carousel': {label: 'Inicio', isDefault: true},
+            //    'quienessomos': {label: 'Quienes somos'},
+            //    'estaciones': {label: 'Estaciones'},
+            //    'pedidos': {label: 'Pedidos'}
+            //  });
+            // oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
 
-      // Navigation setup
-      var navData = [
-      {name: 'Inicio', id: 'inicio',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24', hreff:"#inicio"},
-      {name: 'Servicios', id: 'servicios',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24', hreff:"#servicios"},
-       {name: 'Estaciones', id: 'estaciones',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24', hreff:"#estaciones"},
-      {name: 'Pedidos', id: 'pedidos',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24', hreff:"#pedidos"},
-      
-      ];
-      self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
+            // Navigation setup
+            var navData = [{
+                    name: 'Inicio',
+                    id: 'inicio',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24',
+                    hreff: "#inicio"
+                }, {
+                    name: 'Servicios',
+                    id: 'servicios',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24',
+                    hreff: "#servicios"
+                }, {
+                    name: 'Estaciones',
+                    id: 'estaciones',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24',
+                    hreff: "#estaciones"
+                }, {
+                    name: 'Pedidos',
+                    id: 'pedidos',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24',
+                    hreff: "#pedidos"
+                }, {
+                    name: 'Contacto',
+                    id: 'contacto',
+                    iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24',
+                    hreff: "#contacto"
+                }
 
-      // Drawer
-      // Close offcanvas on medium and larger screens
-      self.mdScreen.subscribe(function() {oj.OffcanvasUtils.close(self.drawerParams);});
-      self.drawerParams = {
-        displayMode: 'push',
-        selector: '#navDrawer',
-        content: '#pageContent'
-      };
-      // Called by navigation drawer toggle button and after selection of nav drawer item
-      self.toggleDrawer = function() {
-        return oj.OffcanvasUtils.toggle(self.drawerParams);
-      }
-      // Add a close listener so we can move focus back to the toggle button when the drawer closes
-      $("#navDrawer").on("ojclose", function() { $('#drawerToggleButton').focus(); });
+            ];
+            self.navDataSource = new oj.ArrayTableDataSource(navData, { idAttribute: 'id' });
 
-      // Header
-      // Application Name used in Branding Area
-      self.appName = ko.observable("Gas Licuado de Sabinas");
-      // User Info used in Global Navigation area
-      self.userLogin = ko.observable("john.hancock@oracle.com");
+            // Drawer
+            // Close offcanvas on medium and larger screens
+            self.mdScreen.subscribe(function() { oj.OffcanvasUtils.close(self.drawerParams); });
+            self.drawerParams = {
+                displayMode: 'push',
+                selector: '#navDrawer',
+                content: '#pageContent'
+            };
+            // Called by navigation drawer toggle button and after selection of nav drawer item
+            self.toggleDrawer = function() {
+                    return oj.OffcanvasUtils.toggle(self.drawerParams);
+                }
+                // Add a close listener so we can move focus back to the toggle button when the drawer closes
+            $("#navDrawer").on("ojclose", function() { $('#drawerToggleButton').focus(); });
+
+            // Header
+            // Application Name used in Branding Area
+            self.appName = ko.observable("Gas Licuado de Sabinas");
+            // User Info used in Global Navigation area
+            self.userLogin = ko.observable("john.hancock@oracle.com");
 
 
 
 
 
 
-      // Footer
-      function footerLink(name, id, linkTarget) {
-        this.name = name;
-        this.linkId = id;
-        this.linkTarget = linkTarget;
-      }
-      self.footerLinks = ko.observableArray([
-        new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
-        new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
-        new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
-        new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
-        new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
-      ]);
-     }  
+            // Footer
+            function footerLink(name, id, linkTarget) {
+                this.name = name;
+                this.linkId = id;
+                this.linkTarget = linkTarget;
+            }
+            self.footerLinks = ko.observableArray([
+                new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
+                new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
+                new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
+                new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+                new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+            ]);
+        }
 
-     return new ControllerViewModel();
-  }
+        return new ControllerViewModel();
+    }
 );
